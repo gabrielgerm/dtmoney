23fd07444dec9bdec23fa4b2256632bd6359438c
Gabriel Oliveira <gabriel-germano333@hotmail.com>
2022-01-25 18:36:33 -0300

    criando botoes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,30 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 
+import Modal from "react-modal";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false)
+    }
+
   return (
     <>
-      <Header />
-      <Dashboard />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />     
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
